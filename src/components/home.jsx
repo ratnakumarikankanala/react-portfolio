2b5ce0d185597b09a3459d49b1dbf043f16e831c
ratnakumarigrownext <ratnakumari@grownext.co>
2026-02-04 13:26:35 +0530

    added message

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,5 +2,8 @@ export const Home =()=>{
     return(<div>
          <h2>Hi, I'm a React.js Developer</h2>
       <p>2 years of frontend experience</p>
+      <p>welcome to my profile
+        
+      </p>
     </div>)
 }
